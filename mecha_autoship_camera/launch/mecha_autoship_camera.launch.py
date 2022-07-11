1821f80ebd9472142055ea4a5fd49224392ef114
varofla <dhksrl0508@naver.com>
2022-07-11 18:47:49 +0900

    modify : run as set namespace

--- mecha_autoship_camera/launch/mecha_autoship_camera.launch.py
+++ mecha_autoship_camera/launch/mecha_autoship_camera.launch.py
@@ -6,8 +6,10 @@ from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
+NAMESPACE = os.environ.get('ROS_NAMESPACE', '')
 
 def generate_launch_description():
+    # 카메라 publishing 노드
     mecha_autoship_image_pub_params = LaunchConfiguration(
         'mecha_autoship_image_pub_params',
         default=os.path.join(
@@ -16,7 +18,21 @@ def generate_launch_description():
             'mecha_autoship_camera.yaml'
         )
     )
+    camera_pub_node_arg = DeclareLaunchArgument(
+        'mecha_autoship_image_pub_params',
+        default_value=mecha_autoship_image_pub_params
+    )
+    camera_pub_node = Node(
+        namespace=NAMESPACE,
+        package='mecha_autoship_camera',
+        executable='mecha_autoship_image_pub_node',
+        name='mecha_autoship_image_pub_node',
+        output='screen',
+        emulate_tty=True,
+        parameters=[mecha_autoship_image_pub_params],
+    )
 
+    # 카메라 필터 노드
     mecha_autoship_image_color_filter_params = LaunchConfiguration(
         'mecha_autoship_image_color_filter_params',
         default=os.path.join(
@@ -25,33 +41,23 @@ def generate_launch_description():
             'mecha_autoship_camera.yaml'
         )
     )
+    camera_filter_node_arg = DeclareLaunchArgument(
+        'mecha_autoship_image_color_filter_params',
+        default_value=mecha_autoship_image_color_filter_params
+    )
+    camera_filter_node = Node(
+        namespace=NAMESPACE,
+        package='mecha_autoship_camera',
+        executable='mecha_autoship_image_color_filter_node',
+        name='mecha_autoship_image_color_filter_node',
+        output='screen',
+        emulate_tty=True,
+        parameters=[mecha_autoship_image_color_filter_params],
+    )
 
     return LaunchDescription([
-        DeclareLaunchArgument(
-            'mecha_autoship_image_pub_params',
-            default_value=mecha_autoship_image_pub_params
-        ),
-
-        Node(
-            package='mecha_autoship_camera',
-            executable='mecha_autoship_image_pub_node',
-            name='mecha_autoship_image_pub_node',
-            output='screen',
-            emulate_tty=True,
-            parameters=[mecha_autoship_image_pub_params],
-        ),
-
-        DeclareLaunchArgument(
-            'mecha_autoship_image_color_filter_params',
-            default_value=mecha_autoship_image_color_filter_params
-        ),
-
-        Node(
-            package='mecha_autoship_camera',
-            executable='mecha_autoship_image_color_filter_node',
-            name='mecha_autoship_image_color_filter_node',
-            output='screen',
-            emulate_tty=True,
-            parameters=[mecha_autoship_image_color_filter_params],
-        ),
+        camera_pub_node_arg,
+        camera_pub_node,
+        camera_filter_node_arg,
+        camera_filter_node
     ])
