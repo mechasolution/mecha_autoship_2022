06de6b4c2601ea234395f402ad79bc291d4d6d7d
varofla <dhksrl0508@naver.com>
2022-07-11 16:43:37 +0900

    modify : use namespace

--- mecha_autoship_teleop/launch/mecha_autoship_joystick.launch.py
+++ mecha_autoship_teleop/launch/mecha_autoship_joystick.launch.py
@@ -1,30 +1,31 @@
 #!/usr/bin/env python3
 
-import os
-
-from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
+NAMESPACE = 'team_mecha'
 
 def generate_launch_description():
-
-    return LaunchDescription([
-        # Joystick Driver
-        Node(
+    # 조이스틱 드라이버 노드
+    joystick_driver_node = Node(
+            namespace=NAMESPACE,
             package='joy',
             executable='joy_node',
             name='joy_node',
             parameters=[],
-            output='screen'),
-
-        # Joystick Node
-        Node(
+            output='screen'
+        )
+    # 조이스틱 데이터 가공 노드
+    joystick_data_node = Node(
+            namespace=NAMESPACE,
             package='mecha_autoship_teleop',
             executable='mecha_autoship_joystick_node',
             name='mecha_autoship_joystick_node',
             parameters=[],
-            output='screen')
+            output='screen'
+        )
+
+    return LaunchDescription([
+        joystick_driver_node,
+        joystick_data_node
     ])
